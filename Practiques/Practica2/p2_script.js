366d3f28e424085e41c32aeb44dd69dc5c2b28c2
Jaume Pla Ferriol <jaumeplaf@gmail.com>
2024-10-28 00:50:28 +0100

    Big updates

--- Practiques/Practica2/p2_script.js
+++ Practiques/Practica2/p2_script.js
@@ -1,35 +1,36 @@
-
 var gl, program;
 
-var exampleTorus;
-
-// transformation vector
-var Tx = 0.5, Ty = 0.5, Tz = 0.0;
+//Declare primitives
+var cube01 = new newObject();
+var cube02 = new newObject();
+var cube03 = new newObject();
+var sphere01 = new newObject();
 
 function getWebGLContext() {
-
+  
   var canvas = document.getElementById("myCanvas");
-
+  
   try {
     return canvas.getContext("webgl2");
   }
   catch(e) {
   }
-
+  
   return null;
-
+  
 }
 
 function initShaders() {
   
   var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  
   gl.shaderSource(vertexShader, document.getElementById("myVertexShader").text);
   gl.compileShader(vertexShader);
   if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
     alert(gl.getShaderInfoLog(vertexShader));
     return null;
   }
- 
+  
   var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
   gl.shaderSource(fragmentShader, document.getElementById("myFragmentShader").text);
   gl.compileShader(fragmentShader);
@@ -48,11 +49,12 @@ function initShaders() {
   
   program.vertexPositionAttribute = gl.getAttribLocation( program, "VertexPosition");
   gl.enableVertexAttribArray(program.vertexPositionAttribute);
-
+  
   //Uniforms
   program.projectionMatrixIndex = gl.getUniformLocation(program,"projectionMatrix");
-  program.modelMatrixIndexS1 = gl.getUniformLocation(program, "modelMatrix");
-
+  program.modelMatrixIndex = gl.getUniformLocation(program, "modelMatrix");
+  program.customColor = gl.getUniformLocation (program, "baseColor" );
+  
 }
 
 function initBuffers(model) {
@@ -71,42 +73,85 @@ function initRendering() {
   
   gl.clearColor(0.0,0.5,0.8,1.0);
   gl.lineWidth(1.5);
+  gl.enable(gl.DEPTH_TEST);
+  //Fix Z-fighting
+  gl.enable(gl.POLYGON_OFFSET_FILL);
+  gl.polygonOffset(0.0, 0.0);
   
 }
 
 function setProjection() {
-    
+  
   //Get the projection perspective's transformation matrix
   var projectionMatrix  = mat4.create();
-
+  
   //Perspective (out, FOVy, aspect, near, far)
-  mat4.perspective(projectionMatrix, Math.PI/4.0, 1.0, 0.01, 1000.0);
+  mat4.perspective(projectionMatrix, Math.PI/4.0, 1.0, 0.1, 1000.0);
   
-  //Send transformation matrix to vertex shader
-  gl.uniformMatrix4fv(program.projectionMatrixIndex,false,projectionMatrix);
+  //Send transformation matrix to vertex shader (location, transpose, value)
+  gl.uniformMatrix4fv(program.projectionMatrixIndex, false, projectionMatrix);
 }
 
-function initPrimitives() {
-
-  initBuffers(examplePlane);
-  initBuffers(exampleCube);
-  initBuffers(exampleCover);
-  initBuffers(exampleCone);
-  initBuffers(exampleCylinder);
-  initBuffers(exampleSphere);
-  
-  // make a torus
-  var innerRadius = 0.1; var outerRadius = 0.8; var nSides = 8; var nRings = 10;
-  exampleTorus = makeTorus (innerRadius, outerRadius, nSides, nRings);
-  initBuffers(exampleTorus);
+function initPrimitives() 
+{  
+    cube01.initializeObject(baseCube);
+    initBuffers(cube01);
+    cube02.initializeObject(baseCube);
+    initBuffers(cube02);
+    cube03.initializeObject(baseCube);
+    initBuffers(cube03);
+    sphere01.initializeObject(baseSphere);
+    initBuffers(sphere01);
 }
-      
+
 function draw(model) {
   
   gl.bindBuffer(gl.ARRAY_BUFFER, model.idBufferVertices);
   gl.vertexAttribPointer(program.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
-
+  
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, model.idBufferIndices);
+
+  gl.drawElements(gl.TRIANGLES, model.indices.length, gl.UNSIGNED_SHORT, 0);
+  
+  for (var i = 0; i < model.indices.length; i += 3){
+    gl.drawElements (gl.LINE_LOOP, 3, gl.UNSIGNED_SHORT, i*2);
+  }
+}
+function drawFlatColor(model, baseColor) {
+  
+  gl.bindBuffer(gl.ARRAY_BUFFER, model.idBufferVertices);
+  gl.vertexAttribPointer(program.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+  
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, model.idBufferIndices);
+  
+  gl.uniform4f (program.customColor, baseColor[0], baseColor[1], baseColor[2], 1.0 );
+  gl.drawElements(gl.TRIANGLES, model.indices.length, gl.UNSIGNED_SHORT, 0);
+}
+function drawWireframe(model, lineColor) {
+  
+  gl.bindBuffer(gl.ARRAY_BUFFER, model.idBufferVertices);
+  gl.vertexAttribPointer(program.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+  
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, model.idBufferIndices);
+  
+  gl.uniform4f (program.customColor, lineColor[0], lineColor[1], lineColor[2], 1.0 );
+
+  for (var i = 0; i < model.indices.length; i += 3){
+    gl.drawElements (gl.LINE_LOOP, 3, gl.UNSIGNED_SHORT, i*2);
+  }
+}
+function drawFlatWireframe(model, baseColor, lineColor) {
+  
+  gl.bindBuffer(gl.ARRAY_BUFFER, model.idBufferVertices);
+  gl.vertexAttribPointer(program.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, model.idBufferIndices);
+
+  gl.polygonOffset(1.0, 1.0);
+  gl.uniform4f (program.customColor, baseColor[0], baseColor[1], baseColor[2], 1.0 );
+  gl.drawElements(gl.TRIANGLES, model.indices.length, gl.UNSIGNED_SHORT, 0);
+  
+  gl.polygonOffset(0.0, 0.0);
+  gl.uniform4f (program.customColor, lineColor[0], lineColor[1], lineColor[2], 1.0 );
   for (var i = 0; i < model.indices.length; i += 3){
     gl.drawElements (gl.LINE_LOOP, 3, gl.UNSIGNED_SHORT, i*2);
   }
@@ -114,49 +159,39 @@ function draw(model) {
 
 function drawScene() {
   
-  gl.clear(gl.COLOR_BUFFER_BIT);
- // load the projection 
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   setProjection();
 
-  //Initilaize ModelView, translation, scale matrix 
-  let M = mat4.create(); 
-  let T = mat4.create();
-  let S = mat4.create ();
+  //Draw with only wireframe
+  cube01.setLineColor([0.0, 0.0, 0.0]);
+  cube01.setMatrix(-1.0, 1.0, -5.0, 1.0);
+  gl.uniformMatrix4fv(program.modelMatrixIndex, false, cube01.modelMatrixIndex);
+  drawWireframe(cube01, cube01.lineColor);
 
-  //Initialize scale and position
-  mat4.fromScaling(S, [1.0, 1.0, 1.0]);
-  mat4.fromTranslation(T,[0, 0, -5.0]);
-  mat4.multiply(M, T, S);
-  
-  mat4.fromTranslation(T,[0.0 , 0.0 , 0.0]);
-  mat4.multiply(M, M, T);
+  //Draw with only flat color
+  cube02.setBaseColor([1,0,0]);
+  cube02.setMatrix(1.0, 1.0, -5.0, 1.0);
+  gl.uniformMatrix4fv(program.modelMatrixIndex, false, cube02.modelMatrixIndex);
+  drawFlatColor(cube02, cube02.baseColor);
 
-  mat4.fromTranslation(T, [1.0, 0.0, 0.0]);
-  mat4.multiply(M, M, T);
-  
-  // load matrix
-  gl.uniformMatrix4fv(program.modelMatrixIndexS1,false,M);
-  // draw
-  draw(exampleSphere);
+  //Draw with both flat color and wireframe
+  cube03.setBaseColor([0,1,0]);
+  cube03.setLineColor([0,0,0]);
+  cube03.setMatrix(-1.0, -1.0, -5.0, 1.0);
+  gl.uniformMatrix4fv(program.modelMatrixIndex, false, cube03.modelMatrixIndex);
+  drawFlatWireframe(cube03, cube03.baseColor, cube03.lineColor);
 
-  mat4.fromScaling(S, [1.0, 1.0, 1.0]);
-  mat4.fromTranslation(T,[0, 0, -5.0]);
-  mat4.multiply(M, T, S);
-  mat4.fromTranslation(T,[-2.0 , 0.0 , 0.0]);
-  mat4.multiply(M, M, T);
-  mat4.fromTranslation(T, [1.0, 0.0, 0.0]);
-  mat4.multiply(M, M, T);
-  gl.uniformMatrix4fv(program.modelMatrixIndexS1,false,M);
-
-  draw(exampleCube);
-
+  sphere01.setBaseColor([1,1,0]);
+  sphere01.setMatrix(1.0, -1.0, -5.0, 0.75);
+  gl.uniformMatrix4fv(program.modelMatrixIndex, false, sphere01.modelMatrixIndex);
+  drawFlatWireframe(sphere01, sphere01.baseColor, sphere01.lineColor);
 
 }
 
 function initWebGL() {
   
   gl = getWebGLContext();
-
+  
   if (!gl) {
     alert("WebGL 2.0 no está disponible");
     return;
@@ -171,3 +206,4 @@ function initWebGL() {
 }
 
 initWebGL();
+
